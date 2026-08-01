435e58d8051d43536179ffb2d1909ab11cf17e22
Natalia Reyes <reyesnatalia453@gmail.com>
2026-08-01 01:43:55 -0500

    Actualiza controlador de plantilla de correo

--- src/services/templateService.js
+++ src/services/templateService.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-
 const getTemplate = (templateName, variables = {}) => {
 
-
+    // Leer el archivo JSON de configuración
     const jsonPath = path.join(
         process.cwd(),
         "public",
@@ -12,84 +11,47 @@ const getTemplate = (templateName, variables = {}) => {
         `${templateName}.json`
     );
 
-
     const json = JSON.parse(
-
-        fs.readFileSync(
-            jsonPath,
-            "utf8"
-        )
-
+        fs.readFileSync(jsonPath, "utf8")
     );
 
-
-
+    // Leer el archivo HTML
     const htmlPath = path.join(
-
         process.cwd(),
-
         "public",
-
         "templates",
-
         json.html
-
     );
-
-
 
     let html = fs.readFileSync(
-
         htmlPath,
-
         "utf8"
-
     );
 
-
-
-    // Primero reemplaza los valores del JSON
-
-    for(const key in json.params){
-
-
+    // Reemplazar los valores por defecto del JSON
+    for (const key in json.params) {
         html = html.replaceAll(
-
             key,
-
             json.params[key]
-
         );
-
-
     }
 
-
-
-   // Luego reemplaza los valores enviados
-Object.keys(variables)
-    .sort((a, b) => b.length - a.length)
-    .forEach((key) => {
-        html = html.replaceAll(key, variables[key]);
-    });
-
-
+    // Reemplazar las variables enviadas desde el controlador
+    for (const key in variables) {
+        html = html.replaceAll(
+            key,
+            variables[key]
+        );
+    }
 
     return {
-
         subject: json.subject,
-
+       
         html
 
     };
-
-
 };
 
-
-
 module.exports = {
-
     getTemplate
-
 };
